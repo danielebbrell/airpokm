e7bbf6ea3247c0d7db0bdfaf7d99582322c08ad0
Francisco Javier Fernandez Mejias <chesco19961@gmail.com>
2023-08-22 19:14:36 +0200

    models and migrations done

--- db/migrate/20230821191039_create_pokemons.rb
+++ db/migrate/20230821191039_create_pokemons.rb
@@ -1,9 +1,12 @@
 class CreatePokemons < ActiveRecord::Migration[7.0]
   def change
     create_table :pokemons do |t|
-      t.name :string
-      t.description :text
+      t.string :name
+      t.text :description
+      t.integer :price
+
       t.timestamps
+
       t.references :user, foreign_key: true
     end
   end
--- db/migrate/20230821193140_create_reviews.rb
+++ db/migrate/20230821193140_create_reviews.rb
@@ -1,9 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
-      t.text :review_text
+      t.string :title
+      t.text :text
+      t.integer :rating
 
       t.timestamps
+
       t.references :booking, foreign_key: true
     end
   end
